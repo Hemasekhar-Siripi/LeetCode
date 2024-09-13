b21f7f92593086d7bcdad69bb454a852fb42432c
Hemasekhar <131686916+Hemasekhar-Siripi@users.noreply.github.com>
2024-09-13 10:39:18 +0530

    Time: 3 ms (100.00%), Space: 43.9 MB (46.55%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.java
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.java
@@ -1,12 +1,14 @@
 class Solution {
     public int rangeBitwiseAnd(int left, int right) {
         int count=0;
+        // Shift left and right until they become the same
         while(left!=right){
             left>>=1;
             right>>=1;
             count+=1;
             
         }
+        // After shifting, left and right will be equal, and we shift left back to its original position
         return left<<=count;
         
     }
