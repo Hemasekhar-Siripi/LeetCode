f17274a5919991adb01df36fd949a795243fd323
Hemasekhar <131686916+Hemasekhar-Siripi@users.noreply.github.com>
2025-07-19 17:41:07 +0530

    Time: 10 ms (65.92%) | Memory: 12.5 MB (81.05%) - LeetSync

---
+++ 38-count-and-say/count-and-say.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        if n == 1:
+            return "1"
+
+        prev = self.countAndSay(n - 1)  # Recursive call
+        result = ""
+        count = 1
+
+        for i in range(1, len(prev)):
+            if prev[i] == prev[i - 1]:
+                count += 1
+            else:
+                result += str(count) + prev[i - 1]
+                count = 1
+
+        # Append the last group
+        result += str(count) + prev[-1]
+
+        return result
